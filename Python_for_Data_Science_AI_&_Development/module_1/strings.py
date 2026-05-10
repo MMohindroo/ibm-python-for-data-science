24d85cb01ee61bca0851ba49999cc35f914ac87a
Manan Mohindroo <mananmohindroo@gmail.com>
2026-05-10 20:23:28 +0530

    beginning of Strings

---
+++ Python_for_Data_Science_AI_&_Development/module_1/strings.py
@@ -0,0 +1,42 @@
+# A string is a sequence of characters
+
+
+# Characters => It can be a letter (Alphabet), Digit (Numbers),
+# Space, Punctation mark or any other symbol
+# we can use single quotes or double quotes for string 
+
+str4 = '123456789'
+Str5 = " 1 2 3 4 5 6 7 8 9"
+str6 = "!@#$%^&*()_+=-:;[]{} <>?/.,"
+
+print(str6)
+
+str1 ="Manan"
+str2 = 'Mohindroo'
+str3 = "Manan Mohindroo"
+
+# In variable str3 , the string length is 15 because 5 letters in first name
+# 9 letters in last name and the space in between then is counted as well
+
+# Each element is the sequence can be accessed using 
+# an index represented by array of numbers
+
+# What is an element ?
+# Each single charecter in the string is one element
+# For "Manan Mohindroo", each letter and the space => Each one is element
+
+# What is indexing ?
+# An index is the position number of each elemant individually. In Python, the index 
+# count starts from 0 not one 
+# So the length of string "Manan Mohindroo" is 15 but index is actully 14 
+# because indexing started from 0 [not 1 to 15 but 0 to 14]
+# Index can also be called as the address of element 
+
+# What is array of numbers?
+
+# Well, the array of numbers just mean the full set of index numbers available in string
+# for "Manan Mohindroo", the array of numbers are 0 to 14 
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+
+
